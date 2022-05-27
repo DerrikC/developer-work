6e24c3c0aa0cf4b2a3e9b5c2b54b6d7b95d0197a
Tammy Campbell <tammycampbell@Tammys-MacBook-Pro.local>
2022-05-27 02:09:44 -0400

    Added popover to copy email button and started customizing the mobile nav

--- src/Components/main.js
+++ src/Components/main.js
@@ -6,10 +6,24 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import { CardActionArea } from "@mui/material";
+import { FiCopy } from "react-icons/fi";
+import Popover from "@mui/material/Popover";
 
 export default function MediaCard() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
     <Container className="main">
       <Card variant="outlined" className="projectsContainer">
@@ -89,6 +103,51 @@ export default function MediaCard() {
             </Stack>
           </CardActions>
         </CardActionArea>
+      </Card>
+      <Card variant="outlined" className="projectsContainer">
+        <CardContent className="description">
+          <Typography gutterBottom variant="h5" component="div">
+            Get in Touch
+          </Typography>
+          <Typography color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+          <Stack className="contact" direction="row" spacing={2}>
+            <Button variant="contained" size="large">
+              Email Me
+            </Button>
+            <Button
+              aria-owns={open ? "mouse-over-popover" : undefined}
+              aria-haspopup="true"
+              onMouseEnter={handlePopoverOpen}
+              onMouseLeave={handlePopoverClose}
+            >
+              <FiCopy size="1.5rem" />
+            </Button>
+            <Popover
+              id="mouse-over-popover"
+              sx={{
+                pointerEvents: "none"
+              }}
+              open={open}
+              anchorEl={anchorEl}
+              anchorPosition={{ top: 600, left: 400 }}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "center"
+              }}
+              transformOrigin={{
+                vertical: "bottom",
+                horizontal: "center"
+              }}
+              onClose={handlePopoverClose}
+              disableRestoreFocus
+            >
+              <Typography sx={{ p: 1 }}>Copy Email Address.</Typography>
+            </Popover>
+          </Stack>
+        </CardContent>
       </Card>
     </Container>
   );
